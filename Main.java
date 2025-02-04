af710db615ec97321786710b592a50f631a3c654
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2025-02-05 00:25:04 +0600

    Calculator using Java

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int n1,n2,operator;
+        System.out.println("1 - Add\n 2 - Suvstract\n 3 - Multiply\n 4 - Divide");
+        System.out.println("Chosse operator :");
+        Scanner sc = new Scanner(System.in);
+        operator = sc.nextInt();
+        System.out.println("Enter first number");
+        n1 = sc.nextInt();
+        System.out.println("Enter Second Number");
+        n2 = sc.nextInt();
+        int result = 0;
+        switch (operator){
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+                result = n1 - n2;
+                break;
+            case 3:
+                result = n1 * n2;
+                break;
+            case 4:
+                result = n1 / n2;
+                break;
+            default:
+                System.out.println("Enter operator is not valid");
+        }
+        System.out.println("result is :" + result);
+
+    }
+}
