488b952c96f44acdbc81e9688c24c2680edbe671
Asif Haque <153304056+AsifHaqe@users.noreply.github.com>
2026-09-18 19:04:37 +0600

    LinearSearchAlgorithm

---
+++ Linear Search/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void linear_search(int a[], int n, int key){
+    int i, count = 0;
+    for(int i = 0;i < n;i++){
+        if(a[i] == key){
+            printf("The element is found at %d position\'n", i + 1);
+            count = count + 1;
+        }
+    }
+    if(count == 0){
+        printf("The element is not present in the array\n");
+    }
+}
+int main(){
+    int i, n, key;
+    n = 6;
+    int a[10] = {12,44,32,18,4,10};
+    key = 18;
+    linear_search(a,n,key);
+}
